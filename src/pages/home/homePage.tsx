387e55d40bf4bb94d0e20fb822cdb3091ae7d276
AngryRaccoon <03sidcrafter@gmail.com>
2024-08-16 16:37:34 +0300

    add some adaptive

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,10 +1,6 @@
 import styles from './homePage.module.css';
 import {Button} from '@gravity-ui/uikit';
 
-/*
-import { Col, Container, Row } from '@gravity-ui/uikit';
-*/
-
 export const HomePage: React.FC = () => {
     return (
         <>
@@ -12,7 +8,7 @@ export const HomePage: React.FC = () => {
                 <div className={styles.headerText}>
                     <h1>
                         Проектная деятельность ИУЦТ! <br /> Что это?
-                    </h1>{' '}
+                    </h1>
                     <p>
                         Проектная деятельность ИУЦТ - это вовлечение студентов в работу по решению
                         практических и исследовательских задач, как в университете, так и за его
@@ -29,34 +25,7 @@ export const HomePage: React.FC = () => {
                     </div>
                 </div>
             </header>
-            <main>
-                {/*                <Container maxWidth="l" className={styles.container}>
-                    <h5 className={styles.title}>О проектной деятельности в ИУЦТ</h5>
-                    <Row
-                        space={{
-                            s: 1,
-                            m: '5',
-                        }}
-                        spaceRow={{
-                            s: 5,
-                            m: '1',
-                        }}
-                    >
-                        <Col s="12" m="6" l="2" xl="3">
-                            <span>jbrfed</span>
-                        </Col>
-                        <Col s="12" m="6" l="2" xl="3">
-                            <span>jbrfed</span>
-                        </Col>
-                        <Col s="6" m="12" xl="3">
-                            <span>jbrfed</span>
-                        </Col>
-                        <Col s="6" m="12" xl="3">
-                            <span>jbrfed</span>
-                        </Col>
-                    </Row>
-                </Container>*/}
-            </main>
+            <main>{/* Additional content here */}</main>
         </>
     );
 };
