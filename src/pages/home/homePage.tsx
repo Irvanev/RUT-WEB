e96c9accc0c8a743588d8a532e57f9c58f632998
AngryRaccoon <03sidcrafter@gmail.com>
2024-08-16 18:15:19 +0300

    add footer and second button is work

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -1,7 +1,12 @@
 import styles from './homePage.module.css';
 import {Button} from '@gravity-ui/uikit';
+import {useNavigate} from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import flagstripe from '../../assets/logo_flagstripe_mini.png';
 
 export const HomePage: React.FC = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <header className={styles.header}>
@@ -16,16 +21,43 @@ export const HomePage: React.FC = () => {
                         учебного процесса.
                     </p>
                     <div className={styles.buttonContainer}>
-                        <Button view="action" size="xl" pin="round-round" width="max">
+                        <Button
+                            view="action"
+                            size="xl"
+                            pin="round-round"
+                            width="max"
+                            onClick={() =>
+                                document.getElementById('project-showcase')?.scrollIntoView()
+                            }
+                        >
                             Витрина проектов
                         </Button>
-                        <Button view="action" size="xl" pin="round-round" width="max">
+                        <Button
+                            view="action"
+                            size="xl"
+                            pin="round-round"
+                            width="max"
+                            onClick={() => navigate('/add')}
+                        >
                             Проектная заявка
                         </Button>
                     </div>
                 </div>
             </header>
-            <main>{/* Additional content here */}</main>
+            <main>
+                <h2>О проектной деятельности в ИУЦТ</h2>
+                <h2>Приемущества проектной деятельности</h2>
+                <h2 id="project-showcase">ВИТРИНА ПРОЕКТОВ</h2>{' '}
+            </main>
+            <footer>
+                <div className={styles.footerCenter}>
+                    <img className={styles.footerImageLogo} src={logo} alt="Logo IMDT" />
+                    <div className={styles.footerText}>
+                        <p>© 2024 ИУЦТ</p>
+                    </div>
+                    <img className={styles.footerImage} src={flagstripe} alt="Logo Flagstripe" />
+                </div>
+            </footer>
         </>
     );
 };
